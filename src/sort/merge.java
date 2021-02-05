08b46fdf3ee7e7d32173ee1254576f807798889c
tunagiant <68101999+tunagiant@users.noreply.github.com>
2021-02-05 17:11:28 +0900

    refactoring

--- src/sort/merge.java
+++ src/sort/merge.java
@@ -14,6 +14,7 @@ public class merge {
 	}
 
 	public static void mergeSort(int start, int end) {
+		// 단위가 한개인 것 방지. 최소단위 두개부터 시작
 		if (start < end) {
 			int mid = (start + end) / 2;
 			// 재귀
@@ -23,13 +24,18 @@ public class merge {
 			int q = mid + 1;
 			int idx = p;
 			while (p <= mid || q <= end) {
-				// 오른쪽분할에 원소가 없는 경우 || 왼쪽분할에 원소가 있고, 오른쪽분할의 원소보다 작을경우
+/* 오른쪽분할의 원소를 다 쓴 경우 || 왼쪽분할에 원소가 있고, 오른쪽분할의 원소보다 작을경우
+ * 왼쪽분할의 원소를 tmp에 채워넣는다. 아닐 시, 오른쪽분할의 원소를 tmp에 채워넣는다.
+ * idx 는 재귀함수 호출에 따라 0부터 end까지 순차적으로 증가함
+ */
 				if (q > end || (p <= mid && src[p] < src[q])) {
 					tmp[idx++] = src[p++];
 				} else {
 					tmp[idx++] = src[q++];
 				}
 			}
+			
+			// 임시배열 tmp를 src에 다시 넣는 과정(재귀에 따라 여러번 갱신됨)
 			for (int i = start; i <= end; i++) {
 				src[i] = tmp[i];
 			}
